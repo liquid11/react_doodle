4a2ef76fcb8cf1a201fd23239d7c15bbc749fc13
arsalantezro <arsalan@tezrosolutions.com>
2018-02-20 03:02:54 +0500

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -4,133 +4,139 @@ import './App.css';
 import Person from "./Person/Person";
 class App extends Component {
 
-  state = {
-    persons: [
-      {
-        name: 'MAX',
-        age: 12
-      }, {
-        name: 'arsaln',
-        age: 2
-      }, {
-        name: 'josh',
-        age: 11
-      }
-    ]
-  }
+    state = {
+        persons: [
+            {
+                name: 'MAX',
+                age: 12
+            }, {
+                name: 'arsaln',
+                age: 2
+            }, {
+                name: 'josh',
+                age: 11
+            }
+        ]
+    }
 
-  switchNameHandler = (newName) => {
+    switchNameHandler = (newName) => {
 
-    // console.log('it is clicked'); wil no revognize //this.state.persons[0].name =
-    // 'ajax';
-    this.setState({
-      persons: [
-        {
-          name: newName,
-          age: 6
-        }, {
-          name: 'john',
-          age: 7
-        }, {
-          name: 'bandit',
-          age: 17
-        }
-      ],
-      otherState: 'the other state',
-      showPersons: false
-    });
+        // console.log('it is clicked'); wil no revognize //this.state.persons[0].name =
+        // 'ajax';
+        this.setState({
+            persons: [
+                {
+                    name: newName,
+                    age: 6
+                }, {
+                    name: 'john',
+                    age: 7
+                }, {
+                    name: 'bandit',
+                    age: 17
+                }
+            ],
+            otherState: 'the other state',
+            showPersons: false
+        });
 
-  }
+    }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        {
-          name: 'Malvin',
-          age: 6
-        }, {
-          name: event.target.value,
-          age: 7
-        }, {
-          name: 'bandit',
-          age: 17
-        }
-      ],
-      otherState: 'the other state'
-    });
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                {
+                    name: 'Malvin',
+                    age: 6
+                }, {
+                    name: event.target.value,
+                    age: 7
+                }, {
+                    name: 'bandit',
+                    age: 17
+                }
+            ],
+            otherState: 'the other state'
+        });
 
-  }
+    }
 
-  togglePersonHandler = () => {
+    togglePersonHandler = () => {
 
- 
-    const doesShow = this.state.showPersons;
-    this.setState({
-      showPersons: !doesShow
-    });
-  }
 
-  render() {
+        const doesShow = this.state.showPersons;
+        this.setState({
+            showPersons: !doesShow
+        });
+    }
 
-    const style = {
+    render() {
 
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1x solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+        const style = {
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {/* <h1 className="App-title">Welcome to React</h1> */}
-        </header>
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1x solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
 
-        {/* <p className="App-intro">
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                    {/* <h1 className="App-title">Welcome to React</h1> */}
+                </header>
+
+                {/* <p className="App-intro">
           bismillah1
           <code>src/App.js</code>
           and save to reload.
         </p> */}
 
-        {/* //click with bind it is recommended*/}
-        {/* <button style={style}
+                {/* //click with bind it is recommended*/}
+                {/* <button style={style}
           onClick={this
           .switchNameHandler
           .bind(this, 'Ragnarok with bind!!')}>switch with bind
         </button>
         <button onClick={() => this.switchNameHandler('Ragnarok with function!')}>switch with function</button> */}
 
-        <button style ={style} onClick={this.togglePersonHandler}>Switch Name</button>
+                <button style ={style} onClick={this.togglePersonHandler}>Switch Name</button>
 
-        {this.state.showPersons
-          ? <div>
-              <Person
-                click={this
-                .switchNameHandler
-                .bind(this, 'Lepo')}
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}></Person>
+                {this.state.showPersons
+                    ? <div>
+                        {this.state.persons.map((person)=>{
 
-              <Person
-                changed={this.nameChangeHandler}
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}>
-                My Hobbies:racing
-              </Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+                            return <Person name={person.name} age={person.age}/>
+
+                        })
+                        }
+                        {/*<Person
+                            click={this
+                                .switchNameHandler
+                                .bind(this, 'Lepo')}
+                            name={this.state.persons[0].name}
+                            age={this.state.persons[0].age}></Person>
+
+                        <Person
+                            changed={this.nameChangeHandler}
+                            name={this.state.persons[1].name}
+                            age={this.state.persons[1].age}>
+                            My Hobbies:racing
+                        </Person>
+                        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>*/}
+                    </div>
+                    : null
+                }
+
             </div>
-          : null
-}
 
-      </div>
-
-    // return
-    // React.createElement('div',{className:'App'},React.createElement('h1',null,'ah
-    // a haha'));
-    );
-  }
+            // return
+            // React.createElement('div',{className:'App'},React.createElement('h1',null,'ah
+            // a haha'));
+        );
+    }
 }
 
 export default App;
